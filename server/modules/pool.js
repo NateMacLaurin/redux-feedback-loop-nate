a9732af3869ae6addb9a1aec1e41ad429a3dc8cf
NateMacLaurin <brow1982@umn.edu>
2021-01-25 09:24:11 -0600

    setup redux reducer, store, middleware, and provider

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -23,6 +23,8 @@ if (process.env.DATABASE_URL) {
         ssl: { rejectUnauthorized: false },
         max: 10, // max number of clients in the pool
         idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+        user: 'postgres',
+        password: 'p3LmANHQ'
     };
 
 } else {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const feedbackReducer = (state = [], action) => {
+    switch(action.type){
+        case 'SET_FEELING':
+            return state;
+        case 'SET_UNDERSTANDING':
+            return state;
+        case 'SET_SUPPORT':    
+            return state;
+        case 'SET_COMMENTS':
+            return state;
+        default:
+            return state;
+    }
+}
+
+const reduxStore = createStore(
+    combineReducers({
+        feedbackReducer
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(<Provider store={reduxStore}<App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
